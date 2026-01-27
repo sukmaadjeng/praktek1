12198f65e650e08a47461dc5ae4699efc4e250dc
Your Name <you@example.com>
2026-01-27 21:22:41 +0700

    hm

--- belajar.php/biodata.php
+++ belajar.php/biodata.php
@@ -1,7 +1,7 @@
 <?php
 $Nama = "Adjeng Purnama Sukma";
 $Tempat_Lahir = "Bandung";
-$Tanggal_Lahir "29 Oktober 2009";
+$Tanggal_Lahir = "29 Oktober 2009";
 $Umur = "16 Tahun";
 $Alamat = "JL. Situ Sipatahunan RT 02, RW 06";
 $Agama = "Islam";
