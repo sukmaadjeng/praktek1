ed48319b970ad8fd2aed33c00bcfe46b8c7470cf
Your Name <you@example.com>
2026-01-28 08:10:52 +0700

    p

--- belajarphp/biodata.php
+++ belajarphp/biodata.php
@@ -1,8 +1,13 @@
 <?php
 $Nama = "Adjeng Purnama Sukma";
-$Tempat_Lahir = "Bandung";
-$Tanggal_Lahir = "29 Oktober 2009";
-$Umur = "16 Tahun";
+$Jurusan = "Teknik Jaringan Komputer Telekomunikasi";
+$Kelas= "XI TJKT 1";
 $Alamat = "JL. Situ Sipatahunan RT 02, RW 06";
 $Agama = "Islam";
+
+echo "Nama : $Nama<br>";
+echo "Jurusan : $Jurusan<br>";
+echo "Kelas : $Kelas<br>";
+echo "Alamat : $Alamat<br>";
+echo "Agama : $Agama<br>";
 ?>
