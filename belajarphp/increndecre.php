7f5776386c1341b179b28dfbb4c5489ff6e3d4fb
Your Name <you@example.com>
2026-01-28 09:17:41 +0700

    he

---
+++ belajarphp/increndecre.php
@@ -0,0 +1,12 @@
+<?php
+$score = 0;
+echo "score awal: $score <br>";
+$score++;
+echo "setelah increment 1: $score <br>";
+$score++;
+echo "setelah increment 2: $score <br>";
+$score++;
+echo "setelah increment 3: $score <br>";
+
+echo $score;
+?>
