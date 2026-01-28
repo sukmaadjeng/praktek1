4b86492be05a05e39761aa8989f40385813d261f
Your Name <you@example.com>
2026-01-28 08:42:10 +0700

    pppp

---
+++ belajarphp/variabel.php
@@ -0,0 +1,13 @@
+<?php
+$Jenis_Kelamin = 'P';
+$Nama_Lengkap = "Adjeng Purnama Sukma";
+$Umur = 16;
+$Berat = 44.8;
+$Tinggi = 155.4;
+
+echo "Nama : $Nama"<br>;
+echo "Jenis Kelamin : $Jenis_Kelamin"<br>;
+echo "Umur : $Umur"<br>;
+echo "Berat : $Berat"<br>;
+echo "Tinggi : $Tinggi"<br>;
+?>
