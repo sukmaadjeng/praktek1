2d0190b0c30e198d0c94f26c968ec5051ed9913a
Your Name <you@example.com>
2026-01-28 09:04:59 +0700

    penugasan

---
+++ belajarphp/penugasan.php
@@ -0,0 +1,10 @@
+<?php
+$speed = 83;
+echo "Speed awal : $speed <br>";
+
+$speed = $speed + 10; 
+echo "Setelah ditambah 10 (aritmatika) : $speed <br>";
+
+$speed += 10;
+echo "setelah ditambah 10 (penugasan) : $speed <br>";
+?>
