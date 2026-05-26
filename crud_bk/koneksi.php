21e06aa6a55a4fa5a661bf2b1a7a86c54e737925
Your Name <you@example.com>
2026-05-26 12:53:04 +0700

    update

--- crud_bk/koneksi.php
+++ crud_bk/koneksi.php
@@ -1,14 +1,14 @@
 <?php
 
 $koneksi = mysqli_connect(
-    "localhost",    // Menambahkan host
-    "2526_01",      // Username database
-    "12345678",     // Memperbaiki tanda petik yang kurang
-    "2526_01_db"    // Menyesuaikan nama database agar tidak ada spasi jika berupa string
+    "localhost",
+    "2526_01",
+    "12345678",
+    "2526_01db"
 );
 
 if(!$koneksi){
-    die("Koneksi gagal: " . mysqli_connect_error());
+    die("Koneksi gagal");
 }
 
 ?>
